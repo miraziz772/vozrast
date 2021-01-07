91043cd4de209659547475fda7ce1f7d80b7bc11
miraziz772 <76822028+miraziz772@users.noreply.github.com>
2021-01-07 15:39:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,250 @@
+var data = +prompt('введите ваш возраст');
+
+switch (data) {
+    case 1:
+        alert('вы еще молоды. нужно учиться')
+        break;
+        case 2:
+            alert('вы еще молоды. нужно учиться')
+            break;
+            case 3:
+                alert('вы еще молоды. нужно учиться')
+                break;
+                case 4:
+                    alert('вы еще молоды. нужно учиться')
+                    break;
+                    case 5:
+                        alert('вы еще молоды. нужно учиться')
+                        break;
+                        case 6:
+                            alert('вы еще молоды. нужно учиться')
+                            break;
+                            case 7:
+                                alert('вы еще молоды. нужно учиться')
+                                break;
+                                case 8:
+                                    alert('вы еще молоды. нужно учиться')
+                                    break;
+                                    case 9:
+                                        alert('вы еще молоды. нужно учиться')
+                                        break;
+                                        case 10:
+                                            alert('вы еще молоды. нужно учиться')
+                                            break;
+                                            case 11:
+                                                alert('вы еще молоды. нужно учиться')
+                                                break;
+                                                case 12:
+                                                    alert('вы еще молоды. нужно учиться')
+                                                    break;
+                                                    case 13:
+                                                        alert('вы еще молоды. нужно учиться')
+                                                        break;
+                                                        case 14:
+                                                            alert('вы еще молоды. нужно учиться')
+                                                            break;
+                                                            case 15:
+                                                                alert('вы еще молоды. нужно учиться')
+                                                                break;
+                                                                case 16:
+                                                                    alert('вы еще молоды. нужно учиться')
+                                                                    break;
+                                                                    case 17:
+                                                                        alert('вы еще молоды. нужно учиться')
+                                                                        break;
+                                                                        case 18:
+                                                                            alert('вы еще молоды. нужно учиться')
+                                                                            break;
+    case 19:
+    alert('вам нужно работать!')
+     break;
+     case 20:
+        alert('вам нужно работать!')
+         break;                                                       
+         case 21:
+            alert('вам нужно работать!')
+             break;
+             case 22:
+    alert('вам нужно работать!')
+     break;  
+     case 23:
+    alert('вам нужно работать!')
+     break;
+     case 24:
+     alert('вам нужно работать!')
+      break;
+      case 25:
+    alert('вам нужно работать!')
+     break;
+     case 26:
+    alert('вам нужно работать!')
+     break;
+     case 27:
+    alert('вам нужно работать!')
+     break;
+     case 28:
+    alert('вам нужно работать!')
+     break;
+     case 29:
+    alert('вам нужно работать!')
+     break;
+     case 30:
+    alert('вам нужно работать!')
+     break;
+     case 31:
+    alert('вам нужно работать!')
+     break;
+     case 32:
+    alert('вам нужно работать!')
+     break;
+     case 33:
+    alert('вам нужно работать!')
+     break;
+     case 34:
+    alert('вам нужно работать!')
+     break;
+     case 35:
+    alert('вам нужно работать!')
+     break;
+     case 36:
+    alert('вам нужно работать!')
+     break;
+     case 37:
+    alert('вам нужно работать!')
+     break;
+     case 38:
+    alert('вам нужно работать!')
+     break;
+     case 39:
+    alert('вам нужно работать!')
+     break;
+     case 40:
+    alert('вам нужно работать!')
+     break;
+     case 41:
+    alert('вам нужно работать!')
+     break;
+     case 42:
+    alert('вам нужно работать!')
+     break;
+     case 43:
+    alert('вам нужно работать!')
+     break;
+     case 44:
+    alert('вам нужно работать!')
+     break;
+     case 45:
+    alert('вам нужно работать!')
+     break;
+     case 46:
+    alert('вам нужно работать!')
+     break;
+     case 47:
+    alert('вам нужно работать!')
+     break;
+     case 48:
+    alert('вам нужно работать!')
+     break;
+     case 49:
+    alert('вам нужно работать!')
+     break;
+     case 50:
+    alert('вам нужно работать!')
+     break;
+     
+     case 51:
+    alert('вам скоро на пенсию')
+     break;
+     case 52:
+    alert('вам скоро на пенсию')
+     break;
+     case 53:
+    alert('вам скоро на пенсию')
+     break;
+     case 54:
+    alert('вам скоро на пенсию')
+     break;
+     case 55:
+    alert('вам скоро на пенсию')
+     break;
+     case 56:
+    alert('вам скоро на пенсию')
+     break;
+     case 57:
+    alert('вам скоро на пенсию')
+     break;
+     case 58:
+    alert('вам скоро на пенсию')
+     break;
+     case 59:
+    alert('вам скоро на пенсию')
+     break;
+
+     case 60:
+    alert('вы пенсионер)')
+     break;
+     case 61:
+    alert('вы пенсионер)')
+     break;
+     case 62:
+        alert('вы пенсионер)')
+         break;
+         case 63:
+            alert('вы пенсионер)')
+             break;
+             case 64:
+                alert('вы пенсионер)')
+                 break;
+                 case 65:
+                    alert('вы пенсионер)')
+                     break;
+                     case 66:
+                        alert('вы пенсионер)')
+                         break;
+                         case 67:
+                            alert('вы пенсионер)')
+                             break;
+                             case 68:
+                                alert('вы пенсионер)')
+                                 break;
+                                 case 69:
+                                    alert('вы пенсионер)')
+                                     break;
+                                     case 70:
+                                        alert('вы пенсионер)')
+                                         break;
+                                         case 71:
+                                            alert('вы пенсионер)')
+                                             break;
+                                             case 72:
+                                                alert('вы пенсионер)')
+                                                 break;
+                                                 case 73:
+                                                    alert('вы пенсионер)')
+                                                     break;
+                                                     case 74:
+                                                        alert('вы пенсионер)')
+                                                         break;
+                                                         case 75:
+                                                            alert('вы пенсионер)')
+                                                             break;
+                                                             case 76:
+                                                                alert('вы пенсионер)')
+                                                                 break;
+                                                                 case 77:
+                                                                    alert('вы пенсионер)')
+                                                                     break;
+                                                                     case 78:
+                                                                        alert('вы пенсионер)')
+                                                                         break;
+                                                                         case 79:
+                                                                            alert('вы пенсионер)')
+                                                                             break;
+                                                                             case 80:
+                                                                                alert('вы пенсионер)')
+                                                                                 break;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+    default:
+        alert('загробный мир - не обслуживаем(')
+        break;
+}
